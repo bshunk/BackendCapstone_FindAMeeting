fe5d4ae248ba0bbd12cfcf17de9bbb57e3e094dc
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-11-06 09:59:15 -0600

    begin working on meeting route and controller so that this backend can connect to the front end

---
+++ routes/meetings.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { } = require('../controllers/meetings');
+
+
+
+// router.post('/auth/register', registerUser);
+// router.post('/auth/login', loginUser);
+// router.get('/auth/user', authHelpers.ensureAuthenticated, getUser);
+
+module.exports = router;
