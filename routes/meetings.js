511b5b66ad58d97225b61e094c9f40550963659c
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-11-06 11:12:18 -0600

    begin setting up routes to link with front end

--- routes/meetings.js
+++ routes/meetings.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { } = require('../controllers/meetings');
 
-
+router.get('/')
 
 // router.post('/auth/register', registerUser);
 // router.post('/auth/login', loginUser);
