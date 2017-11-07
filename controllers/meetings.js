1c0fc9880b839401f172bc29e097459cb4752795
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-11-07 10:30:47 -0600

    work on fixing bug for local port

--- controllers/meetings.js
+++ controllers/meetings.js
@@ -3,7 +3,7 @@ module.exports.getMeetings = (req, res, next) => {
 	const { Meeting } = req.app.get('models');
 	Meeting.findAll({ order: ['id'] })
 	.then( (Meetings) => {
-		res.render('allMeetings', {	Meetings })
+		res.status(200).json(Meetings)
 	})
 	.catch( (err) => {
 		next(err)
