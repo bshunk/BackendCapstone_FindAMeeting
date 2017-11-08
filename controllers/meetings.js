cfcc6a4da47900b8843179e5dcc40c8cb923e72e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-11-08 12:40:25 -0600

    work on getting add new meeting button and functions to work

--- controllers/meetings.js
+++ controllers/meetings.js
@@ -11,3 +11,22 @@ module.exports.getMeetings = (req, res, next) => {
 		next(err)
 	});
 };
+
+module.exports.addNewMeeting = (req, res, next) => {
+	const { Meeting } = req.app.get('models');
+	Meeting.create({
+		category: req.body.category,
+		type: req.body.type,
+		group: req.body.type,
+		city: req.body.city,
+		day: req.body.day,
+		time: req.body.time,
+		location: req.body.location
+	})
+	.then( (data) => {
+		res.status(200).redirect(`/meeting/${data.dataValues.id}`);
+	})
+	.catch( (err) => {
+		res.status(200).json(err);
+	});
+};
