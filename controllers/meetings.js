a18a7cb643ce52ce3d2315b64055fe79df0631ca
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-11-09 11:35:48 -0600

    remove unnecessary console logs and comment code explaining what each functions do in the controller

--- controllers/meetings.js
+++ controllers/meetings.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// called from the front end factory, retrieves meetings from the DB based on what the user has selected for day, time, and city
 module.exports.getMeetings = (req, res, next) => {
   const { Meeting } = req.app.get('models');
 	Meeting.findAll({ where: {day: req.params.day, time: req.params.time, city: req.params.city} })
@@ -12,9 +13,8 @@ module.exports.getMeetings = (req, res, next) => {
 	});
 };
 
+//called from the front end factory, posts a new meeting to the DB based on what the user has filled out in the form and returns the data to a new view
 module.exports.addNewMeeting = (req, res, next) => {
-	// console.log("addNewMeeting Running?");
-	// console.log(req.body);
 	const { Meeting } = req.app.get('models');
 	Meeting.create({
 		category: req.body.category,
@@ -26,14 +26,6 @@ module.exports.addNewMeeting = (req, res, next) => {
 		location: req.body.location
 	})
 	.then( (data) => {
-		// Meeting.findAll({ where: {day: req.params.day, time: req.params.time, city: req.params.city} })
-		// .then( (Meeting) => {
-		// 	res.status(200).json(Meeting)
-		// })
-		// .catch( (err) => {
-		// 	console.log('error', err);
-		// 	next(err)
-		// });
 		res.status(200).render(data.dataValues);
 	})
 	.catch( (err) => {
